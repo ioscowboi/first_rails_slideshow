26a6ed8e84bd38782473b0f24dc2166ca46a32ab
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-20 10:43:24 -0500

    Added gemfile

---
+++ Gemfile
@@ -0,0 +1,17 @@
+
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'rails'
+
+# ///////////////////////////////////////////////////
+# used to set up application for a deploy on heroku:
+gem 'sqlite3', :groups => [:development,:test]
+gem 'pg',  :groups => [:production]
+# end section
+# ///////////////////////////////////////////////////
+
+gem 'pry'
+gem 'rspec'
+gem 'sinatra', '~>1.4.5'
+gem 'sinatra-activerecord'
--- main.rb
+++ main.rb
@@ -11,6 +11,22 @@ DATABASE.results_as_hash = true
 
 require_relative "reel_handler.rb"
 
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+)
+end
+
+configure :development do
+  set :database, {adapter: "sqlite3", database: "slidereel.db"}
+end
+
 # establish database connection: 
 set :database, {adapter: "sqlite3", database: "slidereel.db"}
 
