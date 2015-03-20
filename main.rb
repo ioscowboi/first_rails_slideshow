21198e8043f6255d0f746c3b956befafeb3d279a
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-20 10:35:12 -0500

    Converted application to ActiveRecord::Base

--- main.rb
+++ main.rb
@@ -2,25 +2,28 @@ require "sinatra"
 require 'json'
 require "sqlite3"
 require "pry"
+require "rubygems"
+
+require "sinatra/activerecord"
 
 DATABASE = SQLite3::Database.new("slidereel.db")
 DATABASE.results_as_hash = true
 
-require_relative "reel_handler"
+require_relative "reel_handler.rb"
+
+# establish database connection: 
+set :database, {adapter: "sqlite3", database: "slidereel.db"}
 
 get "/update/:id" do
   new_slide_num = params[:id].to_i
-  @slides = Reel_Handler.all
-  next_slide_hash = @slides[new_slide_num].to_hash
+  @slides = Slide.all
+  next_slide_hash = @slides[new_slide_num]
   json_slide_to_display = next_slide_hash.to_json
 end
 
 get "/" do
-  @slides = Reel_Handler.all
-  slides_hash = @slides.map{ |to_arrays|
-      to_arrays.to_hash
-    }
+  @slides = Slide.all
     @total_slides = @slides.length
-    json_slides = slides_hash.to_json
+    json_slides = @slides
     erb :homepage
 end
